b2994927b149c3fbdee374ab6236def21af1dcfb
gmmoreira2000 <guimoreira10@hotmail.com>
2025-04-03 01:32:59 -0300

    Casos de Teste de Adicionar novo contato

--- Teste de Unidade/AdicionarContatoTeste.cs
+++ Teste de Unidade/AdicionarContatoTeste.cs
@@ -13,35 +13,83 @@ using Api.Controllers;
 using Data.Repository.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Business.Services;
+using Business.ViewModels;
 
 namespace Teste_de_Unidade
 {
     public class AdicionarContatoTeste
     {
-        private IContatoRepository _repositorio;
+        private Mock<IContatoRepository> _repositorio;
+        private ContatoService _contatoService;
         private IMapper _mapper;
 
-        //[Theory]
-        //[InlineData("Nome 1", "2005-01-03", 'F', true)]
-        //[InlineData("Nome 2", "2001-01-03", 'M', true)]
-        //public void AdicionarContatoValido(string nome, DateTime dataNascimento, char sexo, bool ativo)
-        //{
-        //    var option = new DbContextOptionsBuilder<ContatoContextDb>().UseInMemoryDatabase("ContatoContextDb").Options;
-        //    var context = new ContatoContextDb(option);
-        //    var controladorContato = new ContatoController(_repositorio, _mapper);
+        public AdicionarContatoTeste() {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Contato, Business.ViewModels.ContatoViewModel>().ReverseMap();
+            });
+            _mapper = config.CreateMapper();
+            _repositorio = new Mock<IContatoRepository>();
+            _contatoService= new ContatoService(_repositorio.Object, _mapper);
+        }
 
-        //    var contato = new ContatoViewModel();
-        //    contato.Nome = nome;
-        //    contato.DataNascimento = dataNascimento;
-        //    contato.Sexo = sexo;
-        //    contato.Ativo = ativo;
+        [Theory]
+        [InlineData("Nome 1", "2005-01-03", 'F', true)]
+        [InlineData("Nome 2", "2001-07-04", 'M', true)]
+        [InlineData("Nome 3", "2000-02-05", 'T', true)]
+        public async Task AdicionarContatoValido(string nome, DateTime dataNascimento, char sexo, bool ativo)
+        { 
+            var contato = new ContatoViewModel();
+            contato.Nome = nome;
+            contato.DataNascimento = dataNascimento;
+            contato.Sexo = sexo;
+            contato.Ativo = ativo;
 
-        //    IActionResult resultado = null;
+            _repositorio.Setup(r => r.Adicionar(It.IsAny<Contato>()));
 
-        //    //resultado = controladorContato.Adicionar(contato);
 
-        //    Assert.IsType<OkObjectResult>(resultado);
-        //}
+            await _contatoService.Adicionar(contato);
+
+            _repositorio.Verify(r => r.Adicionar(It.IsAny<Contato>()), Times.Once); 
+        }
+
+        [Theory]
+        [InlineData("Nome 1", "2015-01-03", 'F', true)]
+        [InlineData("Nome 2", "2011-07-04", 'M', true)]
+        [InlineData("Nome 3", "2009-02-05", 'T', true)]
+        public async Task AdicionarContatoInValidoIdade(string nome, DateTime dataNascimento, char sexo, bool ativo)
+        {
+            var contato = new ContatoViewModel();
+            contato.Nome = nome;
+            contato.DataNascimento = dataNascimento;
+            contato.Sexo = sexo;
+            contato.Ativo = ativo;
+            _repositorio.Setup(r => r.Adicionar(It.IsAny<Contato>()));
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<Exception>(() => _contatoService.Adicionar(contato));
+            Assert.Equal("A idade do contato deve ser maior de 18", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("Nome 1", "2025-04-27", 'F', true)]
+        [InlineData("Nome 2", "2025-07-04", 'M', true)]
+        [InlineData("Nome 3", "2026-04-03", 'T', true)]
+        public async Task AdicionarContatoInValidoData(string nome, DateTime dataNascimento, char sexo, bool ativo)
+        {
+            var contato = new ContatoViewModel();
+            contato.Nome = nome;
+            contato.DataNascimento = dataNascimento;
+            contato.Sexo = sexo;
+            contato.Ativo = ativo;
+            _repositorio.Setup(r => r.Adicionar(It.IsAny<Contato>()));
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<Exception>(() => _contatoService.Adicionar(contato));
+            Assert.Equal("A data de nascimento não pode ser maior do que a data de hoje", exception.Message);
+        }
 
     }
 }
